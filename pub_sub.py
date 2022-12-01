b95752dd76aa6c1d9bbd6ee62e30826d4a5c5a58
Ola Øren <ola.oren@techseed.no>
2022-12-01 10:38:44 +0100

    Add a minimal mqtt data generator in python.

---
+++ pub_sub.py
@@ -0,0 +1,31 @@
+import time
+import numpy as np
+import paho.mqtt.client as mqtt
+
+broker_url = "127.0.0.1"
+broker_port = 1883
+
+def on_connect(client, userdata, flags, rc):
+   print(f"Connected With Result Code {rc}")
+
+def on_message(client, userdata, message):
+   print("Msg: "+message.payload.decode())
+
+def get_sin(t):
+   return np.sin(t)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(broker_url, broker_port)
+
+while 1:
+   time_seed = time.time()
+   
+   for i in range(1, 10):
+      topic = f"values/sinus{i}"
+      payload = get_sin(time_seed/i)
+      client.publish(topic=topic, payload=payload, qos=0, retain=False)
+
+   time.sleep(0.01)
